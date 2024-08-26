56f133934981f6a83335688734d353d42da5d4fa
pham190vd <pham190vd@prestigein.com>
2024-08-26 10:09:08 +0700

    init commit

---
+++ guide.sh
@@ -0,0 +1,7 @@
+$ terraform init
+$ terraform apply -auto-approve
+# ...
+# Apply complete! Resources: 2 added, 0 changed, 0 destroyed.
+# Outputs:
+# db_password = <sensitive>
+# lb_dns_name = "terraform-series-1259399054.us-west-2.elb.amazonaws.com"
